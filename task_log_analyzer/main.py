a2373f3fa95f9fecda389d39db8a9628eec474d0
n-dett <124851780+n-dett@users.noreply.github.com>
2025-05-03 11:33:39 -0500

    Add home menu

---
+++ task_log_analyzer/main.py
@@ -0,0 +1,45 @@
+def main():
+    print(r"""
+___ ____ ____ _  _    _    ____ ____    ____ ____ _  _ ____ ____ ____ ___ ____ ____ 
+ |  |__| [__  |_/     |    |  | | __    | __ |___ |\ | |___ |__/ |__|  |  |  | |__/ 
+ |  |  | ___] | \_    |___ |__| |__]    |__] |___ | \| |___ |  \ |  |  |  |__| |  \ """, end='')
+
+    # Home menu
+    print(""" 
+―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+Please choose an option by entering a number below:
+1) Upload a .csv file ------------------> (Data from the file will be stored)
+2) View/Edit Task Logs -----------------> (View, add, edit, and delete data)
+3) View Task Log Analytics -------------> (View data summaries and stats)
+    """)
+
+    # Get user input
+    user_num = 0
+    invalid = True
+
+    while invalid:
+        user_num = int(input("Enter a number: "))
+
+        # Check if number is valid
+        if not user_num in (1, 2, 3):
+            print("Invalid number. Please try again.")
+        else:
+            invalid = False
+            print("Reached")
+
+
+    match user_num:
+        case 1:
+            # Go to upload csv screen
+            pass
+        case 2:
+            # Go to view/edit task logs screen
+            pass
+        case 3:
+            # Go to view task log analytics screen
+            pass
+        case _:
+            pass
+
+if __name__ == "__main__":
+    main()
