5bb670bb38579ac745fdde1ace3ac97265d30f26
n-dett <124851780+n-dett@users.noreply.github.com>
2025-05-03 20:03:45 -0500

    Add functions file

--- task_log_analyzer/main.py
+++ task_log_analyzer/main.py
@@ -16,32 +16,33 @@ Please choose an option by entering a number below:
 ** Important **: In order to store and analyze task logs, you must first create\na database instance as specified in the README file
     """)
 
-    # Get user input
-    user_num = 0
-    invalid = True
+    while True:
+        # Get user input
+        user_num = 0
+        invalid = True
 
-    while invalid:
-        user_num = int(input("Enter a number: "))
+        while invalid:
+            user_num = int(input("Enter a number: "))
 
-        # Check if number is valid
-        if not user_num in (1, 2, 3, 4):
-            print("Invalid number. Please try again.")
-        else:
-            invalid = False
+            # Check if number is valid
+            if not user_num in (1, 2, 3, 4):
+                print("Invalid number. Please try again.")
+            else:
+                invalid = False
 
 
-    match user_num:
-        case 1:
-            # Go to upload csv screen
-            pass
-        case 2:
-            # Go to view/edit task logs screen
-            pass
-        case 3:
-            # Go to view task log analytics screen
-            pass
-        case 4:
-            return
+        match user_num:
+            case 1:
+                # Go to upload csv screen
+                pass
+            case 2:
+                # Go to view/edit task logs screen
+                pass
+            case 3:
+                # Go to view task log analytics screen
+                pass
+            case 4:
+                return
 
 if __name__ == "__main__":
     main()
